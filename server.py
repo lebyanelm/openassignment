13ff67cef4d6ca6d83fa87da1cf872a8b1f53d43
lebyanelm <lebyane.lm@gmail.com>
2023-03-20 22:35:03 +0200

    Fix retrry response

--- server.py
+++ server.py
@@ -246,8 +246,8 @@ def recieve_message_prompt():
 				})
 			return Response(cd=200).to_json()
 	except twilio.base.exceptions.TwilioRestException as e:
-		print(e)
-		retry_request = Message(dict(role="user", content="Make it 1500 characters maximum")).__dict__
+		print(e, 'from system.')
+		retry_request = Message(dict(role="user", content=f"{prompt}, summarize it to 120 words.")).__dict__
 		user["messages"].append(retry_request)
 		second_try_response = request_chatgpt_response(user["messages"])
 		send_response_message(to, second_try_response)
@@ -258,7 +258,7 @@ def recieve_message_prompt():
 			})
 		return Response(cd=200).to_json()
 	except:
-		print(traceback.format_exc(), 'response')
+		print(traceback.format_exc())
 		return send_response_message(to, "*Something went wrong when producing the response*, content may be too long / an error occured. Request the message again for a *150 words* response.")
 
 
